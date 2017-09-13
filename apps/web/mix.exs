6d55c09daae3c4c305834be789ad852bbe8fbe4a
Imran Ismail <imran.codely@gmail.com>
2017-09-13 10:32:15 +0800

    Add JSON Codec

--- apps/web/mix.exs
+++ apps/web/mix.exs
@@ -27,6 +27,7 @@ defmodule Web.Mixfile do
     [
       {:cowboy, "~> 1.0.0"},
       {:plug, "~> 1.0"},
+      {:jiffy, "~> 0.14.11"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
