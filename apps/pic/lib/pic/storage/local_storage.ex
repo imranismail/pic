0f3d7505bde680d5eb9bbb44e1d1e978763a9e18
Imran Ismail <imran.codely@gmail.com>
2017-09-13 16:42:15 +0800

    Add bulk upload function to local storage

--- apps/pic/lib/pic/storage/local_storage.ex
+++ apps/pic/lib/pic/storage/local_storage.ex
@@ -2,11 +2,43 @@ defmodule Pic.Storage.LocalStorage do
   @behaviour Pic.Storage
 
   def save(upload, opts) do
-    storage_path = Keyword.fetch!(opts, :at)
+    if upload.content_type == "application/zip" do
+      unzip_and_save(upload, opts)
+    else
+      {:ok, ^upload = perform(upload, opts)}
+    end
+  end
 
+  defp perform(upload, opts) do
+    storage_path = Keyword.fetch!(opts, :at)
     File.mkdir_p!(storage_path)
     File.cp!(upload.path, Path.join([storage_path, "original"]))
+    upload
+  end
 
-    {:ok, upload}
+  defp unzip_and_save(upload, opts) do
+    temp_dir = Path.join([System.tmp_dir!, "pic-storage-local-storage"])
+
+    upload.path
+    |> File.read!()
+    |> :zip.unzip(cwd: temp_dir)
+    |> case do
+      {:ok, paths} ->
+        {:ok, perform_bulk(paths)}
+      {:error, reason} ->
+        raise "#{reason}"
+    end
+  end
+
+  defp perform_bulk(paths) do
+    paths
+    |> Enum.filter(&!String.match?(&1, ~r/\_\_MACOSX/))
+    |> Enum.map(fn path ->
+      Plug.Upload
+      |> struct(content_type: MIME.from_path(path),
+                filename: Path.basename(path, Path.extname(path)),
+                path: path)
+      |> perform(opts)
+    end)
   end
 end
