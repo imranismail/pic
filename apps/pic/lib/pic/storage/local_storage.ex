f8170e4389b4e838698e3d976a3c1e8fe3fc62bb
Imran Ismail <imran.codely@gmail.com>
2017-09-14 14:08:07 +0800

    Update local storage

--- apps/pic/lib/pic/storage/local_storage.ex
+++ apps/pic/lib/pic/storage/local_storage.ex
@@ -3,8 +3,11 @@ defmodule Pic.Storage.LocalStorage do
 
   def save(upload, opts) do
     storage_path = Keyword.fetch!(opts, :at)
+    extension    = MIME.extensions(picture.content_type) |> List.first()
+
     File.mkdir_p(storage_path)
-    File.cp!(upload.path, Path.join([storage_path, "original"]))
+    File.cp!(upload.path, Path.join([storage_path, "original.#{extension}"]))
+
     {:ok, upload}
   end
 end
