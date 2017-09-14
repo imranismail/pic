01bb85d6b521f4a1b7c8b2381271d28161610b79
Imran Ismail <imran.codely@gmail.com>
2017-09-14 14:10:43 +0800

    Fix typo

--- apps/pic/lib/pic/storage/local_storage.ex
+++ apps/pic/lib/pic/storage/local_storage.ex
@@ -3,7 +3,7 @@ defmodule Pic.Storage.LocalStorage do
 
   def save(upload, opts) do
     storage_path = Keyword.fetch!(opts, :at)
-    extension    = MIME.extensions(picture.content_type) |> List.first()
+    extension    = MIME.extensions(upload.content_type) |> List.first()
 
     File.mkdir_p(storage_path)
     File.cp!(upload.path, Path.join([storage_path, "original.#{extension}"]))
